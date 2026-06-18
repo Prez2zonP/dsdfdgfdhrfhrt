a2462e89925c62fdcdf67af624c28d41fe7552b1
Prez2zonP <alisherkapubgmobile@gmail.com>
2026-06-18 16:50:35 +0300

    Add files via upload

---
+++ update_mod_addresses.bat
@@ -0,0 +1,204 @@
+void __fastcall Java_com_blackhub_bronline_game_core_JNIConfig_nativeDestroy(int a1, int a2, void *a3)  ->  sub_642C7C
+bool __fastcall Java_com_blackhub_bronline_game_core_JNIConfig_nativeIsValid(__int64 a1, __int64 a2, _QWORD *a3)  ->  sub_642C8C
+_QWORD *__fastcall Java_com_blackhub_bronline_game_core_JNIConfig_nativeGetChild( __int64 a1, __int64 a2, _QWORD *a3, __int64 a4)  ->  sub_642CA8
+__int64 __fastcall Java_com_blackhub_bronline_game_core_JNIConfig_nativeGetIntPacked( __int64 a1, __int64 a2, __int64 *a3)  ->  sub_642DFC
+float __fastcall Java_com_blackhub_bronline_game_core_JNIConfig_nativeGetFloat(__int64 a1, __int64 a2, __int64 *a3)  ->  sub_642EA0
+__int64 __fastcall Java_com_blackhub_bronline_game_core_JNIConfig_nativeGetString(__int64 a1, __int64 a2, __int64 *a3)  ->  sub_642F58
+__int64 __fastcall Java_com_blackhub_bronline_game_core_JNIConfig_nativeSetInt( __int64 a1, __int64 a2, __int64 a3, int a4)  ->  sub_6430A0
+__int64 __fastcall Java_com_blackhub_bronline_game_core_JNIConfig_nativeSetFloat( float a1, __int64 a2, __int64 a3, __int64 a4)  ->  sub_643128
+__int64 __fastcall Java_com_blackhub_bronline_game_core_JNIConfig_nativeSetString( __int64 a1, int a2, __int64 a3, __int64 a4, int a5, int a6, int a7, int a8, int a9, void *a10, int a11, int a12, char a13)  ->  sub_6431C4
+_QWORD *Java_com_blackhub_bronline_game_core_JNIConfig_nativeGameSettings()  ->  sub_643390
+__int64 __fastcall Java_com_blackhub_bronline_game_core_keyboardHelper_SoftwareKeyboardBridge_nativeOnKeyUp( __int64 a1, __int64 a2, int a3)  ->  sub_7460C8
+__int64 __fastcall Java_com_blackhub_bronline_game_core_keyboardHelper_SoftwareKeyboardBridge_nativeOnKeyDown( __int64 a1, __int64 a2, int a3)  ->  sub_746184
+void __fastcall Java_com_blackhub_bronline_game_core_keyboardHelper_SoftwareKeyboardBridge_nativeCommitText( __int64 a1, __int64 a2, __int64 a3, unsigned int a4, unsigned int a5)  ->  sub_746240
+__int64 __fastcall Java_com_blackhub_bronline_game_core_keyboardHelper_SoftwareKeyboardBridge_nativeOnSetVisible( __int64 a1, __int64 a2, char a3)  ->  sub_7465B8
+__int64 __fastcall Java_com_blackhub_bronline_game_core_keyboardHelper_SoftwareKeyboardBridge_nativeOnKeyboardSizeChanged( __int64 a1, __int64 a2, unsigned int a3, unsigned int a4)  ->  sub_74667C
+__int64 __fastcall std::bad_any_cast::bad_any_cast(std::bad_any_cast *this)  ->  sub_C738D8
+__int64 __fastcall Java_com_blackhub_bronline_launcher_di_HelpshiftManager_nativeInit(__int64 a1, __int64 a2)  ->  sub_C74430
+void __fastcall Java_com_blackhub_bronline_game_core_JNILib_sendJsonData( __int64 a1, int a2, int a3, __int64 a4, int a5, int a6, int a7, int a8, int a9, int a10, int a11, int a12, int a13, void *a14, int a15, int a16)  ->  sub_C74B80
+__int64 __fastcall Java_com_blackhub_bronline_game_core_JNILib_sendUpdateSystemCodedMessage( __int64 a1, __int64 a2, int a3, int a4, __int64 a5)  ->  sub_C82350
+__int64 __fastcall Java_com_blackhub_bronline_game_core_JNILib_tryGetPatchIndex( __int64 a1, __int64 a2, __int64 a3, __int64 a4, char a5, int a6, int a7, int a8, int a9, char a10, char a11, char a12)  ->  sub_C8334C
+UpdateManagerSystemApi *__fastcall Java_com_blackhub_bronline_game_core_JNILib_getAdditionDownloadPatchData( int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, char a9, int a10, void *a11)  ->  sub_C83870
+bool __fastcall Java_com_blackhub_bronline_game_core_JNILib_tryDownloadResources( __int64 a1, __int64 a2, char a3, int a4, char a5, int a6, int a7)  ->  sub_C83938
+bool __fastcall Java_com_blackhub_bronline_game_core_JNILib_tryDownloadNextSlot( __int64 a1, __int64 a2, char a3, int a4, char a5, int a6, int a7, __int64 a8, char a9)  ->  sub_C8396C
+__int64 __fastcall Java_com_blackhub_bronline_game_core_JNILib_getCurrentPatchVersion( UpdateManagerSystemApi *a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, char a9, int a10, void *a11)  ->  sub_C84308
+__int64 __fastcall Java_com_blackhub_bronline_game_core_JNILib_getTargetPatchVersion( UpdateManagerSystemApi *a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, char a9, int a10, void *a11)  ->  sub_C843C4
+__int64 __fastcall Java_com_blackhub_bronline_game_core_JNILib_start( __int64 *a1, float a2, __int64 a3, int a4, int a5, int a6, int a7, __int64 a8, __int64 a9, unsigned int a10, char a11)  ->  sub_C84480
+__int64 __fastcall Java_com_blackhub_bronline_game_core_JNILib_orientationChanged( __int64 a1, __int64 a2, int a3, int a4, int a5)  ->  sub_C846E8
+void Java_com_blackhub_bronline_game_core_JNILib_removeLastCheckDateMarker()  ->  sub_C85488
+__int64 Java_com_blackhub_bronline_game_core_JNILib_cancelDownloadResources_0()  ->  sub_C8BA78
+__int64 __fastcall mz_stream_open(__int64 (***a1)(void))  ->  sub_DCC71C
+__int64 __fastcall mz_stream_is_open(__int64 a1)  ->  sub_DCC73C
+__int64 __fastcall mz_stream_read_uint64(__int64 *a1, _QWORD *a2)  ->  sub_DCCBC4
+__int64 __fastcall mz_stream_write(__int64 *a1, __int64 a2, unsigned int a3)  ->  sub_DCCD0C
+__int64 __fastcall mz_stream_write_uint16(__int64 *a1, __int64 a2)  ->  sub_DCCE50
+__int64 __fastcall mz_stream_write_int64(__int64 *a1, __int64 a2)  ->  sub_DCCF90
+__int64 __fastcall mz_stream_write_uint64(__int64 *a1, __int64 a2)  ->  sub_DCD030
+__int64 __fastcall mz_stream_tell(__int64 *a1)  ->  sub_DCD5B8
+__int64 __fastcall mz_stream_close(__int64 *a1)  ->  sub_DCDD90
+__int64 __fastcall mz_stream_error(__int64 a1)  ->  sub_DCDDFC
+__int64 __fastcall mz_stream_set_base(__int64 a1, __int64 a2)  ->  sub_DCDE1C
+__int64 __fastcall mz_stream_get_interface(__int64 result)  ->  sub_DCDE2C
+__int64 __fastcall mz_stream_get_prop_int64(__int64 a1)  ->  sub_DCDE38
+__int64 __fastcall mz_stream_set_prop_int64(__int64 a1)  ->  sub_DCDE58
+__int64 __fastcall mz_stream_create(__int64 a1)  ->  sub_DCDE78
+__int64 **__fastcall mz_stream_delete(__int64 **result)  ->  sub_DCDE90
+__int64 __fastcall mz_stream_raw_is_open(__int64 a1)  ->  sub_DCDEDC
+int64x2_t __fastcall mz_stream_raw_read(int64x2_t *a1, __int64 a2, unsigned int a3)  ->  sub_DCDF00
+int64x2_t __fastcall mz_stream_raw_write(int64x2_t *a1, __int64 a2, unsigned int a3)  ->  sub_DCDFE4
+__int64 __fastcall mz_stream_raw_tell(__int64 a1)  ->  sub_DCE0B8
+__int64 __fastcall mz_stream_raw_seek(__int64 a1, __int64 a2, unsigned int a3)  ->  sub_DCE128
+__int64 __fastcall mz_stream_raw_error(__int64 a1)  ->  sub_DCE1DC
+__int64 __fastcall mz_stream_raw_get_prop_int64(__int64 a1, int a2, _QWORD *a3)  ->  sub_DCE200
+__int64 __fastcall mz_stream_raw_set_prop_int64(__int64 a1, int a2, __int64 a3)  ->  sub_DCE240
+_QWORD *mz_stream_raw_create()  ->  sub_DCE260
+void __fastcall mz_stream_raw_delete(void **a1)  ->  sub_DCE28C
+_BYTE *mz_zip_create()  ->  sub_DCE2BC
+void __fastcall mz_zip_delete(void **a1)  ->  sub_DCE2EC
+__int64 __fastcall mz_zip_open(__int64 a1, __int64 a2, int a3)  ->  sub_DCE31C
+__int64 __fastcall mz_zip_close(__int64 a1)  ->  sub_DCED3C
+__int64 __fastcall mz_zip_entry_is_open(__int64 a1)  ->  sub_DCF18C
+__int64 __fastcall mz_zip_entry_close(__int64 a1)  ->  sub_DCF1AC
+__int64 __fastcall mz_zip_get_comment(__int64 a1, _QWORD *a2)  ->  sub_DCF1E8
+__int64 __fastcall mz_zip_set_comment(__int64 a1, const char *a2)  ->  sub_DCF214
+__int64 __fastcall mz_zip_get_version_madeby(__int64 a1, _WORD *a2)  ->  sub_DCF2A4
+__int64 __fastcall mz_zip_set_version_madeby(__int64 a1, __int16 a2)  ->  sub_DCF2C4
+__int64 __fastcall mz_zip_set_recover(__int64 a1, char a2)  ->  sub_DCF2E0
+__int64 __fastcall mz_zip_set_data_descriptor(__int64 a1, char a2)  ->  sub_DCF2FC
+__int64 __fastcall mz_zip_get_stream(__int64 a1, _QWORD *a2)  ->  sub_DCF318
+__int64 __fastcall mz_zip_set_cd_stream(_QWORD *a1, __int64 a2, __int64 a3)  ->  sub_DCF340
+__int64 __fastcall mz_zip_get_cd_mem_stream(__int64 a1, _QWORD *a2)  ->  sub_DCF364
+__int64 __fastcall mz_zip_set_number_entry(__int64 a1, __int64 a2)  ->  sub_DCF38C
+__int64 __fastcall mz_zip_get_number_entry(__int64 a1, _QWORD *a2)  ->  sub_DCF3A8
+__int64 __fastcall mz_zip_set_disk_number_with_cd(__int64 a1, int a2)  ->  sub_DCF3C8
+__int64 __fastcall mz_zip_get_disk_number_with_cd(__int64 a1, _DWORD *a2)  ->  sub_DCF3E4
+__int64 __fastcall mz_zip_entry_read_open(__int64 a1, char a2, __int64 a3)  ->  sub_DCF404
+__int64 __fastcall mz_zip_entry_seek_local_header(__int64 a1)  ->  sub_DCF570
+__int64 __fastcall mz_zip_entry_write_open(__int64 a1, __int64 a2, unsigned int a3, char a4, __int64 a5)  ->  sub_DD0160
+__int64 __fastcall mz_zip_attrib_is_dir(unsigned int a1, __int16 a2)  ->  sub_DD0644
+__int64 __fastcall mz_zip_time_t_to_dos_date(__int64 a1)  ->  sub_DD06DC
+__int64 __fastcall mz_zip_entry_read(__int64 a1, __int64 a2, unsigned int a3)  ->  sub_DD125C
+__int64 __fastcall mz_zip_entry_write(__int64 a1, __int64 a2, unsigned int a3)  ->  sub_DD12EC
+__int64 __fastcall mz_zip_entry_read_close(__int64 a1, unsigned int *a2, __int64 *a3, __int64 *a4)  ->  sub_DD1370
+__int64 __fastcall mz_zip_extrafield_contains(__int64 a1, int a2, __int16 a3, unsigned __int16 *a4)  ->  sub_DD15C8
+__int64 __fastcall mz_zip_entry_write_close(__int64 a1, unsigned int a2, __int64 a3, __int64 a4)  ->  sub_DD1840
+__int64 __fastcall mz_zip_entry_get_compress_stream(__int64 a1, _QWORD *a2)  ->  sub_DD1E24
+__int64 __fastcall mz_zip_entry_close_raw(__int64 a1, __int64 a2, unsigned int a3)  ->  sub_DD1E4C
+__int64 __fastcall mz_zip_entry_is_symlink(__int64 a1)  ->  sub_DD1F80
+__int64 __fastcall mz_zip_attrib_is_symlink(unsigned int a1, __int16 a2)  ->  sub_DD2024
+__int64 __fastcall mz_zip_entry_get_info(__int64 a1, _QWORD *a2)  ->  sub_DD20BC
+__int64 __fastcall mz_zip_entry_get_local_info(__int64 a1, _QWORD *a2)  ->  sub_DD20E8
+__int64 __fastcall mz_zip_entry_set_extrafield(__int64 a1, __int64 a2, __int16 a3)  ->  sub_DD2110
+__int64 __fastcall mz_zip_goto_entry(__int64 a1, __int64 a2)  ->  sub_DD214C
+__int64 __fastcall mz_zip_goto_first_entry(__int64 a1)  ->  sub_DD21F0
+__int64 __fastcall mz_zip_goto_next_entry(__int64 a1)  ->  sub_DD2274
+__int64 __fastcall mz_zip_locate_entry(__int64 a1, __int64 a2, char a3)  ->  sub_DD2314
+__int64 __fastcall mz_zip_path_compare(unsigned __int8 *a1, __int64 a2, char a3)  ->  sub_DD2458
+__int64 __fastcall mz_zip_locate_first_entry( __int64 a1, __int64 a2, __int64 (__fastcall *a3)(__int64, __int64, __int64))  ->  sub_DD256C
+__int64 __fastcall mz_zip_attrib_convert(unsigned __int8 a1, unsigned int a2, unsigned __int8 a3, unsigned int *a4)  ->  sub_DD2770
+__int64 __fastcall mz_zip_attrib_win32_to_posix(__int16 a1, int *a2)  ->  sub_DD2898
+__int64 __fastcall mz_zip_attrib_posix_to_win32(__int16 a1, int *a2)  ->  sub_DD28E4
+__int64 __fastcall mz_zip_extrafield_find(__int64 *a1, unsigned __int16 a2, int a3, unsigned __int16 *a4)  ->  sub_DD293C
+__int64 __fastcall mz_zip_extrafield_read(__int64 *a1, _WORD *a2, _WORD *a3)  ->  sub_DD2A14
+__int64 __fastcall mz_zip_extrafield_write(__int64 *a1, __int64 a2, unsigned int a3)  ->  sub_DD2A60
+__int64 __fastcall mz_zip_dosdate_to_tm(unsigned int a1, __int64 a2)  ->  sub_DD2A9C
+__int64 __fastcall mz_zip_dosdate_to_time_t(unsigned int a1, double a2, double a3, double a4, uint32x4_t a5)  ->  sub_DD2B28
+__int64 __fastcall mz_zip_time_t_to_tm(__int64 a1, __int64 a2)  ->  sub_DD2BC4
+__int64 __fastcall mz_zip_tm_to_dosdate(_DWORD *a1)  ->  sub_DD2C7C
+__int64 __fastcall mz_zip_ntfs_to_unix_time(__int64 a1, unsigned __int64 *a2)  ->  sub_DD2D18
+__int64 __fastcall mz_zip_unix_to_ntfs_time(__int64 a1, __int64 *a2)  ->  sub_DD2D50
+const char *__fastcall mz_zip_get_compression_method_string(int a1)  ->  sub_DD2D78
+__int64 __fastcall mz_stream_mem_open(__int64 a1, __int64 a2, int a3)  ->  sub_DD2E9C
+__int64 __fastcall mz_stream_mem_is_open(__int64 a1)  ->  sub_DD2F3C
+__int64 __fastcall mz_stream_mem_read(__int64 a1, void *dest, signed int a3)  ->  sub_DD2F50
+__int64 __fastcall mz_stream_mem_write(__int64 a1, const void *a2, signed int a3)  ->  sub_DD2FD8
+__int64 __fastcall mz_stream_mem_tell(__int64 a1)  ->  sub_DD30C0
+__int64 __fastcall mz_stream_mem_set_buffer(__int64 result, __int64 a2, int a3)  ->  sub_DD31CC
+__int64 __fastcall mz_stream_mem_get_buffer(__int64 a1, _QWORD *a2)  ->  sub_DD31D8
+__int64 __fastcall mz_stream_mem_get_buffer_at(__int64 a1, __int64 a2, _QWORD *a3)  ->  sub_DD3200
+__int64 __fastcall mz_stream_mem_get_buffer_at_current(__int64 a1, _QWORD *a2)  ->  sub_DD3234
+__int64 __fastcall mz_stream_mem_get_buffer_length(__int64 result, _DWORD *a2)  ->  sub_DD3270
+__int64 __fastcall mz_stream_mem_set_buffer_limit(__int64 result, int a2)  ->  sub_DD327C
+__int64 __fastcall mz_stream_mem_set_grow_size(__int64 result, int a2)  ->  sub_DD3284
+void __fastcall mz_stream_mem_delete(__int64 *a1)  ->  sub_DD32C0
+__int64 __fastcall mz_stream_zlib_open(__int64 a1, __int64 a2, int a3)  ->  sub_DD3324
+__int64 __fastcall mz_stream_zlib_is_open(__int64 a1)  ->  sub_DD33E0
+__int64 mz_stream_zlib_tell()  ->  sub_DD360C
+__int64 __fastcall mz_stream_zlib_error(__int64 a1)  ->  sub_DD370C
+__int64 __fastcall mz_stream_zlib_get_prop_int64(__int64 a1, int a2, _QWORD *a3)  ->  sub_DD3718
+__int64 __fastcall mz_stream_zlib_set_prop_int64(__int64 a1, int a2, __int64 a3)  ->  sub_DD3780
+_WORD *mz_stream_zlib_create()  ->  sub_DD37E0
+void __fastcall mz_stream_zlib_delete(void **a1)  ->  sub_DD3820
+__int64 (__fastcall **mz_stream_zlib_get_interface())()  ->  sub_DD3850
+void __fastcall mz_os_utf8_string_delete(void **a1)  ->  sub_DD3860
+__int64 __fastcall mz_os_rand(void *a1, size_t a2)  ->  sub_DD3890
+__int64 __fastcall mz_os_rename(const char *a1, const char *a2)  ->  sub_DD38D0
+__int64 __fastcall mz_os_unlink(const char *a1)  ->  sub_DD38F0
+__int64 __fastcall mz_os_file_exists(const char *a1)  ->  sub_DD3910
+__int64 __fastcall mz_os_get_file_size(const char *a1)  ->  sub_DD3978
+__int64 __fastcall mz_os_get_file_date(const char *a1, _QWORD *a2, _QWORD *a3, _QWORD *a4)  ->  sub_DD3A00
+__int64 __fastcall mz_os_set_file_date(const char *a1, __int64 a2, struct utimbuf a3)  ->  sub_DD3AE4
+__int64 __fastcall mz_os_set_file_attribs(const char *a1, __mode_t a2)  ->  sub_DD3BB0
+__int64 __fastcall mz_os_make_dir(const char *a1)  ->  sub_DD3BD0
+bool __fastcall mz_os_is_dir_separator(char a1)  ->  sub_DD3C48
+__int64 __fastcall mz_os_is_dir(const char *a1)  ->  sub_DD3C58
+__int64 __fastcall mz_os_is_symlink(const char *a1)  ->  sub_DD3CC8
+__int64 __fastcall mz_os_make_symlink(char *to, char *from)  ->  sub_DD3D38
+__int64 __fastcall mz_os_read_symlink(__int64 a1, __int64 a2, int a3)  ->  sub_DD3D64
+unsigned __int64 mz_os_ms_time()  ->  sub_DD3DB4
+__int64 __fastcall mz_path_combine(char *s, const char *a2, int a3)  ->  sub_DD3E24
+__int64 __fastcall mz_path_append_slash(const char *a1, int a2, char a3)  ->  sub_DD3F14
+__int64 __fastcall mz_path_remove_slash(const char *a1)  ->  sub_DD3FAC
+__int64 __fastcall mz_path_has_slash(const char *a1)  ->  sub_DD4004
+__int64 __fastcall mz_path_convert_slashes(const char *a1, char a2)  ->  sub_DD4050
+__int64 __fastcall mz_path_compare_wc(_BYTE *a1, unsigned __int8 *a2, char a3)  ->  sub_DD40B8
+__int64 __fastcall mz_path_resolve(char *a1, char *a2, int a3)  ->  sub_DD4210
+__int64 __fastcall mz_path_remove_filename(const char *a1)  ->  sub_DD4460
+__int64 __fastcall mz_path_remove_extension(const char *a1)  ->  sub_DD44D8
+__int64 __fastcall mz_path_get_filename(char *a1, _QWORD *a2)  ->  sub_DD4564
+__int64 __fastcall mz_dir_make(const char *a1)  ->  sub_DD45D4
+__int64 __fastcall mz_stream_os_seek(__int64 a1, __off_t a2, unsigned int a3)  ->  sub_DD48A8
+__int64 __fastcall mz_stream_os_is_open(__int64 a1)  ->  sub_DD48F4
+__int64 __fastcall mz_stream_os_read(__int64 a1, void *ptr, signed int a3)  ->  sub_DD4908
+__int64 __fastcall mz_stream_os_write(__int64 a1, void *ptr, signed int a3)  ->  sub_DD4970
+__off_t __fastcall mz_stream_os_tell(__int64 a1)  ->  sub_DD49D8
+__int64 __fastcall mz_stream_os_close(__int64 a1)  ->  sub_DD4A24
+__int64 __fastcall mz_stream_os_error(__int64 a1)  ->  sub_DD4A68
+_QWORD *mz_stream_os_create()  ->  sub_DD4A70
+void __fastcall mz_stream_os_delete(void **a1)  ->  sub_DD4A9C
+__int64 (__fastcall **mz_stream_os_get_interface())(int, char *filename)  ->  sub_DD4ACC
+__int64 __fastcall mz_stream_pkcrypt_open(__int64 a1, _BYTE *a2, char a3)  ->  sub_DD4AD8
+__int64 __fastcall mz_stream_pkcrypt_is_open(__int64 a1)  ->  sub_DD4F8C
+__int64 __fastcall mz_stream_pkcrypt_write(__int64 a1, char *a2, int a3)  ->  sub_DD50C0
+__int64 __fastcall mz_stream_pkcrypt_tell(__int64 a1)  ->  sub_DD5230
+__int64 __fastcall mz_stream_pkcrypt_seek(__int64 a1, __int64 a2, unsigned int a3)  ->  sub_DD5238
+__int64 __fastcall mz_stream_pkcrypt_close(__int64 a1)  ->  sub_DD5240
+__int64 __fastcall mz_stream_pkcrypt_error(__int64 a1)  ->  sub_DD5250
+__int64 __fastcall mz_stream_pkcrypt_set_password(__int64 result, __int64 a2)  ->  sub_DD5258
+__int64 __fastcall mz_stream_pkcrypt_set_verify(__int64 result, char a2, char a3, __int16 a4)  ->  sub_DD5268
+__int64 __fastcall mz_stream_pkcrypt_get_verify(__int64 result, _BYTE *a2, _BYTE *a3, _WORD *a4)  ->  sub_DD527C
+__int64 __fastcall mz_stream_pkcrypt_get_prop_int64(__int64 a1, int a2, __int64 *a3)  ->  sub_DD529C
+__int64 __fastcall mz_stream_pkcrypt_set_prop_int64(__int64 a1, int a2, __int64 a3)  ->  sub_DD5304
+_QWORD *mz_stream_pkcrypt_create()  ->  sub_DD532C
+void __fastcall mz_stream_pkcrypt_delete(void **a1)  ->  sub_DD535C
+__int64 (__fastcall **mz_stream_pkcrypt_get_interface())()  ->  sub_DD538C
+__int64 __fastcall INT123_frame_outbuffer(__int64 a1)  ->  sub_E4D15C
+__int64 __fastcall opus_packet_parse(unsigned __int8 *a1, int a2, _BYTE *a3, char **a4, int16x8_t *a5, _DWORD *a6)  ->  sub_E72374
+void *zng_get_crc_table()  ->  sub_EBD6F0
+__int64 __fastcall zng_crc32_z(__int64 a1, __int64 a2, __int64 a3)  ->  sub_EBD6FC
+__int64 __fastcall zng_crc32(__int64 a1, __int64 a2, unsigned int a3)  ->  sub_EBD714
+__int64 __fastcall zng_deflateInit(_QWORD *a1, unsigned int a2)  ->  sub_EBDCA8
+__int64 __fastcall zng_deflateInit_(_QWORD *a1, unsigned int a2, _BYTE *a3, int a4)  ->  sub_EBDCBC
+__int64 __fastcall zng_deflateInit2_( _QWORD *a1, unsigned int a2, int a3, unsigned int a4, int a5, unsigned int a6, _BYTE *a7, int a8)  ->  sub_EBDCF0
+__int64 __fastcall zng_deflate(__int64 a1, __int64 a2)  ->  sub_EBE72C
+__int64 __fastcall zng_inflateInit(_QWORD *a1)  ->  sub_EC5448
+__int64 __fastcall zng_inflateInit_(_QWORD *a1, _BYTE *a2, int a3)  ->  sub_EC5450
+__int64 __fastcall zng_inflate(__int64 a1, __int64 a2, unsigned __int64 a3, unsigned __int64 a4, __int16 *a5)  ->  sub_EC5548
+__int64 __fastcall zng_inflateCodesUsed(__int64 a1)  ->  sub_EC80A0
+char *__fastcall zng_zError(int a1)  ->  sub_ECA5D0
+long double __fastcall std::__shared_mutex_base::__shared_mutex_base(std::__shared_mutex_base *this)  ->  sub_ECA744
+__int64 __fastcall std::__shared_mutex_base::lock(std::__shared_mutex_base *this)  ->  sub_ECA764
+bool __fastcall std::shared_timed_mutex::try_lock_shared(std::shared_timed_mutex *this)  ->  sub_ECAB64
+__int64 __fastcall std::regex_error::regex_error(std::runtime_error *this, int a2)  ->  sub_ECAC28
+__int64 __fastcall std::__match_any_but_newline<char>::__exec(__int64 result, __int64 a2)  ->  sub_ECAE2C
+__int64 __fastcall std::__match_any_but_newline<wchar_t>::__exec(__int64 result, __int64 a2)  ->  sub_ECAE7C
